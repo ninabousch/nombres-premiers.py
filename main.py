a2540f04fd7e392d9287457a6b58c84d89561c3e
nina.bouschbacher <ninabousch44@gmail.com>
2025-09-22 13:38:23 +0000

    nombres premiers

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
-from math import sqrt
-
-#### Fonction secondaire
-
-
+""" Module principal pour la verification et l'afficahge des nombres premiers"""
+#isprime.py
+### fonction secondaire
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
+    """renvoie True si p vaut 1 ou 0 """
+    e={0,1}
+    if p in e:
+        return False
+    for i in range (2,p):
+        if p%i==0:
+            return False
+    return True
 #### Fonction principale
-
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """teste la fonction isprime """
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
-    print()
-
-
-if __name__ == "__main__":
-    main()
+            print()
+    if __name__ == "__main__":
+        main()
